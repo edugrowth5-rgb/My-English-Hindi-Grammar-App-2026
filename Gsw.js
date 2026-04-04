9f05db846640b5e541e7db05943a7df413e24e9f
edugrowth5-rgb <edugrowth5@gmail.com>
2026-04-04 23:57:45 +0530

    Create Gsw.js

---
+++ Gsw.js
@@ -0,0 +1,47 @@
+/**
+ * Gsw.js - Service Worker for Grammar App
+ * Files to cache for Offline access
+ */
+
+const cacheName = 'grammar-app-v1';
+const assets = [
+  '/',
+  '/Gindex.html',
+  '/Gstyle.css',
+  '/Gapp.js',
+  '/Gsentence.js',
+  '/Gsangya.js',
+  '/GpartsOfSpeech.js',
+  // यहाँ अपनी अन्य टॉपिक फाइल्स और लोगो के नाम भी जोड़ें
+];
+
+// 1. Install Event: फाइल्स को स्टोर करना
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('Caching App Shell Assets... 📁');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// 2. Activate Event: पुराने कैश को साफ़ करना
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// 3. Fetch Event: ऑफलाइन होने पर स्टोर की गई फाइल दिखाना
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
